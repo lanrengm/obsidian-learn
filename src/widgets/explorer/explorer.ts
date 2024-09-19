a21eddaa6339bbb2640d6d2512c5720edca421ab
lanrengm <lanrengm@gmail.com>
2024-09-19 23:47:57 +0800

    新增 ZoneFolder，ZoneFile 类

--- src/widgets/explorer/explorer.ts
+++ src/widgets/explorer/explorer.ts
@@ -89,7 +89,7 @@ class View extends ItemView {
   navigation: boolean = false;
   // 记录当前选中的文件或文件夹
   // 实现两次点击同一个文件夹展开
-  focusedEl: HTMLElement | null = null;
+  focusedItem: HTMLElement | null = null;
 
   getViewType(): string {
     return VIEW_TYPE;
@@ -152,12 +152,12 @@ class View extends ItemView {
           'stroke-linejoin': 'round',
           'class': 'svg-icon right-triangle'
         }
-      })
+      });
       svgIcon.createSvg('path', {
         attr: {
           'd': 'M3 8L12 17L21 8'
         }
-      })
+      });
       let treeItemInner = treeItemSelf.createDiv({
         text: t.name,
         cls: ['tree-item-inner', 'nav-folder-title-content']
@@ -166,7 +166,7 @@ class View extends ItemView {
       // 点击目录的事件
       // 第一次点击是选中，第二次点击是展开子目录，第三次点击是收起子目录
       treeItemSelf.onClickEvent(evt => {
-        if (this.focusedEl === treeItemSelf) {
+        if (this.focusedItem === treeItemSelf) {
           // 点击已选中文件夹, 等同于双击
           if (treeItem.children.length === 1) {
             // 展开子目录
@@ -183,12 +183,11 @@ class View extends ItemView {
             treeItem.children[1].remove();
           }
         } else {
-          // 点击新的文件夹
-          // 取消其它文件或目录的选中
-          this.focusedEl?.removeClasses(['is-active', 'has-focus']);
+          // 点击新的文件夹, 取消其它文件或目录的选中
+          this.focusedItem?.removeClasses(['is-active', 'has-focus']);
           // 选中当前目录
           treeItemSelf.addClasses(['is-active', 'has-focus']);
-          this.focusedEl = treeItemSelf;
+          this.focusedItem = treeItemSelf;
         }
       });
     });
@@ -214,19 +213,132 @@ class View extends ItemView {
         // is-active 是外边框变化，可以用键盘方向键控制，作用是光标指示器。
         // has-focus 是背景变化，鼠标单击选中，或方向键切换is-active后按回车选中，作用是指示当前正在编辑的文件。
         // 取消其它文件或目录的选中
-        this.focusedEl?.removeClasses(['is-active', 'has-focus']);
+        this.focusedItem?.removeClasses(['is-active', 'has-focus']);
         // 选中当前文件
         treeItemSelf.addClasses(['is-active', 'has-focus']);
-        this.focusedEl = treeItemSelf;
+        this.focusedItem = treeItemSelf;
       });
     })
   }
+}
 
-  openFolder(t: TFolder): void {
+class ZoneFolder {
+  view: View;
+  t: TFolder;
+  treeItem: HTMLElement;
+  treeItemSelf: HTMLElement;
+  treeItemIcon: HTMLElement;
+  treeItemChildren: HTMLElement | null = null;
 
+  constructor(view: View, t: TFolder, parent: HTMLElement) {
+    this.view = view;
+    this.t = t;
+    this.treeItem = parent.createDiv({
+      cls: ['tree-item', 'nav-folder', 'is-collapsed']
+    });
+    this.treeItemSelf = this.treeItem.createDiv({
+      cls: [
+        'tree-item-self', 'is-clickable',
+        'nav-folder-title', 'mod-collapsible'
+      ]
+    });
+    this.createIcon();
+    this.createText();
+    // 点击目录的事件
+    // 第一次点击当前目录是选中，第二次点击是展开子目录，第三次点击是收起子目录
+    this.treeItemSelf.onClickEvent(evt => {
+      if (this.view.focusedItem !== this.treeItemSelf) {
+        // 第一次点击当前文件夹, 取消其它文件或目录的选中
+        this.view.focusedItem?.removeClasses(['is-active', 'has-focus']);
+        this.treeItemSelf.addClasses(['is-active', 'has-focus']);
+        this.view.focusedItem = this.treeItemSelf;
+      } else {
+        // 再次点击当前文件夹，展开或折叠目录
+        !this.treeItemChildren ? this.expand() : this.collapse();
+      }
+    })
+  }
+  
+  // 展开子目录
+  expand() {
+    this.treeItem.removeClass('is-collapsed')
+    this.treeItemIcon.removeClass('is-collapsed');
+    this.treeItemChildren = this.treeItem.createDiv({
+      cls: ['tree-item-children', 'nav-folder-children']
+    });
+    this.view.showFolderToEl(this.t.path, this.treeItemChildren);
   }
 
-  closeFolder(t: TFolder): void {
+  // 收起子目录
+  collapse() {
+    this.treeItem.addClass('is-collapsed');
+    this.treeItemIcon.addClass('is-collapsed');
+    this.treeItemChildren?.remove();
+    this.treeItemChildren = null;
+  }
 
+  private createIcon() {
+    this.treeItemIcon = this.treeItemSelf.createDiv({ 
+      cls: ['tree-item-icon', 'nav-folder-collapse-indicator', 'collapse-icon', 'is-collapsed']
+    });
+    // 模仿文件管理器部分的 【展开/收起】 图标
+    // '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon right-triangle"><path d="M3 8L12 17L21 8"></path></svg>'
+    let svgIcon = this.treeItemIcon.createSvg('svg', { attr: {
+      'xmlns': 'http://www.w3.org/2000/svg',
+      'width': '24',
+      'height': '24',
+      'viewBox': '0 0 24 24',
+      'fill': 'none',
+      'stroke': 'currentColor',
+      'stroke-width': '2',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round',
+      'class': 'svg-icon right-triangle'
+    }});
+    svgIcon.createSvg('path', { attr: {
+      'd': 'M3 8L12 17L21 8'
+    }});
+  }
+
+  private createText() {
+    this.treeItemSelf.createDiv({
+      text: this.t.name,
+      cls: ['tree-item-inner', 'nav-folder-title-content']
+    });
+  }
+}
+
+class ZoneFile {
+  view: View;
+  t: TFile;
+  
+  constructor(view: View, t: TFile, parent: HTMLElement) {
+    this.view = view;
+    this.t = t;
+
+    let treeItem = parent.createDiv({
+      cls: ['tree-item', 'nav-file']
+    });
+    let treeItemSelf = treeItem.createDiv({
+      cls: [
+        'tree-item-self', 'is-clickable', 
+        'nav-file-title', 'tappable'
+      ]
+    });
+    let treeItemInner = treeItemSelf.createDiv({
+      text: t.name,
+      cls: ['tree-item-inner', 'nav-file-title-content']
+    });
+
+    // 点击文件的事件
+    treeItem.onClickEvent(evt => {
+      // is-active 是外边框变化，可以用键盘方向键控制，作用是光标指示器。
+      // has-focus 是背景变化，鼠标单击选中，或方向键切换is-active后按回车选中，作用是指示当前正在编辑的文件。
+      // 取消其它文件或目录的选中
+      this.view.focusedItem?.removeClasses(['is-active', 'has-focus']);
+      // 选中当前文件
+      treeItemSelf.addClasses(['is-active', 'has-focus']);
+      this.view.focusedItem = treeItemSelf;
+    });
   }
 }
